362f711e8231e9d5b6436f0989b79275aa67b632
Lizosy <lizwozmain@gmail.com>
2023-10-04 22:20:35 +0700

    001

--- test/in.js
+++ test/in.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Event handler for the email input
   $('.email').on("input", function() {
     if ($(this).val()) {
       $('.icon-paper-plane').addClass("next");
@@ -7,16 +8,19 @@ $(document).ready(function() {
     }
   });
 
+  // Add cursor pointer on hover for next buttons
   $('.next-button').hover(function() {
     $(this).css('cursor', 'pointer');
   });
 
+  // Event handler for the email next button
   $('.next-button.email').click(function() {
     console.log("Something");
     $('.email-section').addClass("fold-up");
     $('.password-section').removeClass("folded");
   });
 
+  // Event handler for the password input
   $('.password').on("input", function() {
     if ($(this).val()) {
       $('.icon-lock').addClass("next");
@@ -25,23 +29,44 @@ $(document).ready(function() {
     }
   });
 
+  // Event handler for the password next button
   $('.next-button.password').click(function() {
     console.log("Something");
     $('.password-section').addClass("fold-up");
     $('.repeat-password-section').removeClass("folded");
   });
 
+  // Event handler for the repeat password input
   $('.repeat-password').on("input", function() {
     if ($(this).val()) {
       $('.icon-repeat-lock').addClass("next");
+      $('.next-button.repeat-password').css('pointer-events', 'auto'); // Enable the button
     } else {
       $('.icon-repeat-lock').removeClass("next");
+      $('.next-button.repeat-password').css('pointer-events', 'none'); // Disable the button
     }
   });
 
+  // Event handler for the repeat password next button
   $('.next-button.repeat-password').click(function() {
     console.log("Something");
     $('.repeat-password-section').addClass("fold-up");
     $('.success').css("marginTop", 0);
+  });
+
+  // Event handler for the reset button
+  $('.reset-button').click(function() {
+    // Clear form fields and reset button states
+    $('.email').val('');
+    $('.password').val('');
+    $('.repeat-password').val('');
+    $('.animated-button .next-button').removeClass('next');
+  });
+
+  // Event handler for the refresh icon within the repeat password section
+  $('.icon-repeat-lock i').click(function() {
+    // Perform the same reset behavior as clicking the "fa fa-paper-plane" icon
+    $('.repeat-password-section').addClass('fold-up');
+    $('.success').css('marginTop', 0);
   });
 });
